06efebdf1ae473dc50abe69c2a7f8f54f11723c8
kimhyunjin <khjzzm@gmail.com>
2025-03-14 17:10:34 +0900

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // ✅ Next.js App Router 사용 시 필요
+    "./src/pages/**/*.{js,ts,jsx,tsx}", // ✅ 기존 Pages Router 사용 시 필요
+    "./src/components/**/*.{js,ts,jsx,tsx}", // ✅ 컴포넌트 폴더 적용
+    "./src/layout/**/*.{js,ts,jsx,tsx}", // ✅ 레이아웃 관련 폴더 적용
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
